887900fd3599d637a02fd12c0af1f506ff641102
Kevin Hogan <kchogan@pacbell.net>
2024-01-24 15:35:03 -0800

    added array of answer objects for testing and Shape class tests

--- test/svg.test.js
+++ test/svg.test.js
@@ -1,27 +1,25 @@
 const svg = require("../lib/svg");
-const utils = require("../lib/utils");
-
-const answers = utils.answers;
+const answers = require("../lib/answers");
 
 describe("Make SVG", () => {
   describe("Fonts", () => {
     it("should add a generic backup font when provided a font family", () => {
-      const oldFontFamily = answers.fontFamily;
-      svg.makeSVG(answers);
-      const newFontFamily = answers.fontFamily;
+      const oldFontFamily = answers[0].fontFamily;
+      svg.makeSVG(answers[0]);
+      const newFontFamily = answers[0].fontFamily;
       expect(newFontFamily).toEqual(oldFontFamily + ", san-serif");
     });
   });
   describe("Border Color", () => {
     it("should assign the main shape color if stroke color is undefined", () => {
-      svg.makeSVG(answers);
-      expect(answers.strokeColor).toEqual(answers.shapeColor);
+      svg.makeSVG(answers[0]);
+      expect(answers[0].strokeColor).toEqual(answers[0].shapeColor);
     });
   });
   describe("Hex Colors", () => {
     it("should verify that hex colors start with '#'", () => {
-      svg.makeSVG(answers);
-      expect(answers.textColor[0]).toEqual("#");
+      svg.makeSVG(answers[0]);
+      expect(answers[0].textColor[0]).toEqual("#");
     });
   });
 });
